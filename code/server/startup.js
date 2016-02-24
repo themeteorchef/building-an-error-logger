d454d8a26faf962f6989076f9583fcbfe2f34a50
Ryan Glover <me@ryanglover.net>
2016-02-24 13:50:35 -0600

    add a demo function to auto log errors

--- code/server/startup.js
+++ code/server/startup.js
@@ -4,4 +4,16 @@ import seedDatabase from './modules/seed-database';
 Meteor.startup( () => {
   setBrowserPolicies();
   seedDatabase();
+
+  // Just for demo purposes. You can remove this :)
+  Meteor.setInterval( () => {
+    WhaHappened.error({
+      title: 'I don\'t think so!',
+      message: 'This is just a test message that\'s automatically being inserted every 30 seconds. You can delete me at the line and file noted below.',
+      payload: {
+        line: '9-16',
+        file: '/server/startup.js'
+      }
+    });
+  }, 30000 );
 });
